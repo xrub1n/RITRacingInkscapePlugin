02e43cee79f5a74913455b69b111822045604257
melowtonin <saw7555@rit.edu>
2025-11-15 12:57:42 -0500

    Hard coding working, now just have to fix the options

--- set_box_prototype1_extension.py
+++ set_box_prototype1_extension.py
@@ -3,17 +3,17 @@ import inkex
 
 class SetBoxPrototype1Extension(inkex.Effect):
     def add_arguments(self, pars):
-        """Add extension parameters."""
+        """Add extension parameters."""  
         pars.add_argument("--text_content", type=str, default="Sample Text", help="Text to display")
         pars.add_argument("--box_color_green", type=inkex.Boolean, default=True, help="Box fill color")
         pars.add_argument("--box_color_blue", type=inkex.Boolean, default=False, help="Box fill color")
         pars.add_argument("--box_color_pink", type=inkex.Boolean, default=False, help="Box fill color")
         pars.add_argument("--border_black_or_white", type=inkex.Boolean, default=False, help="Border black or white")
-        pars.add_argiment("--green_color", type=inkex.color, default=inkex.color("#2596be33"), help="Color green")  #setting each color up as a variable so it can be passed into other functions
-        pars.add_argiment("--blue_color", type=inkex.color, default=inkex.color("blue"), help="Color blue")
-        pars.add_argiment("--pink_color", type=inkex.color, default=inkex.color("pink"), help="Color pink")
-        pars.add_argiment("--white_color", type=inkex.color, default=inkex.color("white"), help="Color white")
-        pars.add_argiment("--black_color", type=inkex.color, default=inkex.color("black"), help="Color black")
+        pars.add_argument("--green_color", type=inkex.Color, default=inkex.Color("#b6d6a8"), help="Color green")  #setting each color up as a variable so it can be passed into other functions
+        pars.add_argument("--blue_color", type=inkex.Color, default=inkex.Color("#2596be"), help="Color blue")
+        pars.add_argument("--pink_color", type=inkex.Color, default=inkex.Color("#9d579"), help="Color pink")
+        pars.add_argument("--white_color", type=inkex.Color, default=inkex.Color("#FFFFFF"), help="Color white")
+        pars.add_argument("--black_color", type=inkex.Color, default=inkex.Color("#000000"), help="Color black")
         pars.add_argument("--box_width", type=int, default=60, help="Box width")
         pars.add_argument("--box_height", type=int, default=20, help="Box height")
         pars.add_argument("--text_size", type=int, default=7.5, help="Font size")
@@ -34,6 +34,8 @@ class SetBoxPrototype1Extension(inkex.Effect):
         box_width = self.options.box_width
         box_height = self.options.box_height
         text_size = self.options.text_size
+
+
         
         SVG_NS = "{http://www.w3.org/2000/svg}"
 
@@ -49,22 +51,22 @@ class SetBoxPrototype1Extension(inkex.Effect):
             'y': '0',
             'width': str(box_width),
             'height': str(box_height),
-            'style': f'fill:{color_green};stroke:{color_black};stroke-width:5' #default as green, then try to swap the color depending on if the user selected something else
+            'style': f'fill:{green_color};stroke:{black_color};stroke-width:1' #default as green, then try to swap the color depending on if the user selected something else
 
-            if box_color_blue:
-                elem.style.set_color(self.options.color_blue, 'fill')  #if blue was picked, swap the fill color to blue
-            else:
-                pass  #otherwise just skip the function
+            # if box_color_blue:
+            #     elem.style.set_color(self.options.color_blue, 'fill')  #if blue was picked, swap the fill color to blue
+            # else:
+            #     pass  #otherwise just skip the function
             
-            if box_color_pink:
-                elem.style.set_color(self.options.color_pink, 'fill') #same as blue
-            else:
-                pass  #see blue else
+            # if box_color_pink:
+            #     elem.style.set_color(self.options.color_pink, 'fill') #same as blue
+            # else:
+            #     pass  #see blue else
 
-            if border_black_or_white:    #defaults to already being black
-                pass                        
-            else:
-                elem.style.set_color(self.options.color_white, 'stroke')   #if the user selects the box it turns the border white. Swap this from bool later
+            # if border_black_or_white:    #defaults to already being black
+            #     pass                        
+            # else:
+            #     elem.style.set_color(self.options.color_white, 'stroke')   #if the user selects the box it turns the border white. Swap this from bool later
         }
         inkex.etree.SubElement(group, SVG_NS + 'rect', rect_attribs)
 
